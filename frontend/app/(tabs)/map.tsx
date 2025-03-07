01a913691707086effeb0864243fde3e67676e14
RickBigBalls <ricky123max@gmail.com>
2025-03-07 19:41:49 +0800

    added map page
    still in development, needs to add auto gps location access, display image on map, and changable map styles

--- frontend/app/(tabs)/map.tsx
+++ frontend/app/(tabs)/map.tsx
@@ -1,11 +1,13 @@
 import { Text, View, StyleSheet } from 'react-native';
+import React from 'react';
+import MapView from 'react-native-maps';
 
 export default function Map() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Map</Text>
-    </View>
-  );
+	return (
+		<View style={{ flex: 1 }}>
+			<MapView style={StyleSheet.absoluteFill} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
