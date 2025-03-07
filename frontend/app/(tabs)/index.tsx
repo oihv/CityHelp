1cc979707efd27589f824bd2126eeffd299d4fb5
oihv <kokolas16@gmail.com>
2025-03-07 18:27:58 +0800

    Add header with logo
    Resolve merge conflict

--- frontend/app/(tabs)/index.tsx
+++ frontend/app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
+import { Header, Image, SearchBar } from 'react-native-elements';
+import Logo from '@/assets/images/CityHelpLogo.svg';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
@@ -7,14 +9,21 @@ import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
+    //<ParallaxScrollView
+    //  headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+    //  headerImage={
+    //    <Image
+    //      source={require('@/assets/images/partial-react-logo.png')}
+    //      style={styles.reactLogo}
+    //    />
+    //  }>
+    <View>
+      <Header
+        backgroundColor='#5087F7'
+        leftComponent={
+          <Logo width={50} height={50} />
+        }
+      />
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">城市助手</ThemedText>
         <HelloWave />
@@ -50,7 +59,11 @@ export default function HomeScreen() {
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+      <Image
+        source={require('@/assets/images/CityHelpText.svg')}
+        style={styles.logo}
+      />
+    </View>
   );
 }
 
@@ -64,11 +77,10 @@ const styles = StyleSheet.create({
     gap: 8,
     marginBottom: 8,
   },
-  reactLogo: {
+  logo: {
     height: 178,
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
   },
 });
