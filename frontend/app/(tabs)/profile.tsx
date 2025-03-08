519368c0cd053c664a0672a809467d9257984df8
oihv <kokolas16@gmail.com>
2025-03-08 22:24:23 +0800

    Revert changes on profile, as it was for testing image purposes

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -5,8 +5,11 @@ import { Text, View, StyleSheet, Image, TouchableOpacity, Dimensions } from 'rea
 const { width, height } = Dimensions.get('window'); 
 =======
 import { Text, View, StyleSheet } from 'react-native';
+<<<<<<< HEAD
 import {Image} from 'react-native';
 >>>>>>> e4a0633 (Add buttons in the home page using the new HomeButton component)
+=======
+>>>>>>> 8b30ab7 (Revert changes on profile, as it was for testing image purposes)
 
 export default function Profile() {
   const handleButtonPress = (buttonName: string) => {
@@ -61,9 +64,12 @@ export default function Profile() {
       </View>
 =======
       <Text style={styles.text}>Profile</Text>
+<<<<<<< HEAD
       <Image 
       source={require('@/assets/images/MapIcon.png')} />
 >>>>>>> e4a0633 (Add buttons in the home page using the new HomeButton component)
+=======
+>>>>>>> 8b30ab7 (Revert changes on profile, as it was for testing image purposes)
     </View>
   );
 }
