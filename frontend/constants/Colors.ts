ca1cc8e697dd5e8e308e6f216169199f573f1160
oihv <kokolas16@gmail.com>
2025-03-08 20:17:13 +0800

    Change colors to be similar to figma design

--- frontend/constants/Colors.ts
+++ frontend/constants/Colors.ts
@@ -9,7 +9,9 @@ const tintColorDark = '#fff';
 export const Colors = {
   light: {
     text: '#11181C',
-    background: '#fff',
+    background: '#E4EBF6',
+    elementBackground: '#FFFFFF',
+    buttonBackground: '#EAFBFF',
     tint: tintColorLight,
     icon: '#687076',
     tabIconDefault: '#687076',
